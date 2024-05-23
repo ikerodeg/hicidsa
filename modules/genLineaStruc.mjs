22333fac46433402846139790acb8ebe7135ce2b
Iker <ocp2k.android@gmail.com>
2024-05-23 12:58:09 +0200

    Add modules file
    Añade estructura final

--- modules/genLineaStruc.mjs
+++ modules/genLineaStruc.mjs
@@ -13,16 +13,60 @@ function articleLineaJornada(globalObject, lineaBaseInfoMatch){
   return `
     <article class="article__jornada">
       <h2 class="article__h2">Inicio Jornada</h2>
-      <p class="article__p">📍 <a target="_blank" href='${'#'}'>${lineaBaseInfoMatch.rutas[globalObject.SELECTED_DIAS_id].buses[globalObject.SELECTED_BUS_id].turno[globalObject.SELECTED_TURNO_id].lgInicio} 🔗</a></p>
+      <p class="article__p">📍 <a target="_blank" href='${'https://maps.app.goo.gl/2jGinLz6hphgikEW9'}'>${lineaBaseInfoMatch.rutas[globalObject.SELECTED_DIAS_id].buses[globalObject.SELECTED_BUS_id].turno[globalObject.SELECTED_TURNO_id].lgInicio} 🔗</a></p>
       <p class="article__p article__p--hrs">${lineaBaseInfoMatch.rutas[globalObject.SELECTED_DIAS_id].buses[globalObject.SELECTED_BUS_id].turno[globalObject.SELECTED_TURNO_id].hrInicio}</p>
       <h2 class="article__h2">Relevo / Fin Jornada</h2>
-      <p class="article__p">📍 <a target="_blank" href='${'#'}'>${lineaBaseInfoMatch.rutas[globalObject.SELECTED_DIAS_id].buses[globalObject.SELECTED_BUS_id].turno[globalObject.SELECTED_TURNO_id].lgRelevo} 🔗</a></p>
+      <p class="article__p">📍 <a target="_blank" href='${'https://maps.app.goo.gl/Ham3U6XMtEsxbaJh7'}'>${lineaBaseInfoMatch.rutas[globalObject.SELECTED_DIAS_id].buses[globalObject.SELECTED_BUS_id].turno[globalObject.SELECTED_TURNO_id].lgRelevo} 🔗</a></p>
       <p class="article__p article__p--hrs">${lineaBaseInfoMatch.rutas[globalObject.SELECTED_DIAS_id].buses[globalObject.SELECTED_BUS_id].turno[globalObject.SELECTED_TURNO_id].hrRelevo}</p>
     </article>
   `
 };
 
 function articleLineaBilletes(globalObject, lineaBaseInfoMatch){
+  if (lineaBaseInfoMatch.maquina.observaciones) {
+    return `
+    <article class="article__billetes">
+      <h2 class="article__h2">Códigos Billetes</h2>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> interurbano</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bInterUrbano}</span> ${lineaBaseInfoMatch.maquina.bInterUrbanoPrec}</p>
+      </div>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> urbano</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bUrbano}</span> ${lineaBaseInfoMatch.maquina.bUrbanoPrec}</p>
+      </div>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> jubilados</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bJubilados}</span> ${lineaBaseInfoMatch.maquina.bJubiladosPrec}</p>
+      </div>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> < 3 años</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bmenores}</span> ${lineaBaseInfoMatch.maquina.bmenoresPrec}</p>
+      </div>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> < 7 años</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bhasta7}</span> ${lineaBaseInfoMatch.maquina.bhasta7Prec}</p>
+      </div>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> transbordo</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bTransbordo}</span> ${lineaBaseInfoMatch.maquina.bTransbordoPrec}</p>
+      </div>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> desc. general</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bDescGeneral}</span> ${lineaBaseInfoMatch.maquina.bDescGeneralPrec}</p>
+      </div>
+      <div>
+        <p class="article__p"><span class="span__icon">🎟️</span> desc. especial</p>
+        <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.bDescEspecial}</span> ${lineaBaseInfoMatch.maquina.bDescEspecialPrec}</p>
+      </div>
+      <div>
+      <p class="article__p">Cambio tipo de billete</p>
+      <p class="article__p"><span class="span__key">${lineaBaseInfoMatch.maquina.keyChangeBillete}</span></p>
+      <p class="article__p">${lineaBaseInfoMatch.maquina.observaciones}</p>
+    </div>
+    </article>
+  `
+  }
   return `
     <article class="article__billetes">
       <h2 class="article__h2">Códigos Billetes</h2>
@@ -93,9 +137,8 @@ function articleLineaHorarios(globalObject, lineaBaseInfoMatch){
   `;
 };
 
-
-function sectionStops(globalObject, rutasObject){
-  const stopsRuta = rutasObject[globalObject.SELECTED_COLEGIO_id][globalObject.ALL_SELECTIONS].stops.map ( (parada, index) => `<li><a href="${parada.linkMaps}">${index + 1}. ${parada.name}<br></a>${parada.descript}</li>`).join('');
+function articleLineaStops(globalObject, lineaBaseInfoMatch, rutasObject){
+  const stopsRuta = rutasObject[globalObject.SELECTED_LINEA_id][globalObject.ALL_SELECTIONS].stops.map ( (parada, index) => `<li><a target="_blank" href="${parada.linkMaps}">${index + 1}. ${parada.name}<br></a>${parada.descript}</li>`).join('');
 
   return `
     <article class="article__stops">
@@ -103,24 +146,23 @@ function sectionStops(globalObject, rutasObject){
       <ul class="article__ul">${stopsRuta}</ul>
     </article>
   `
-
 };
 
-function  sectionLinkRuta(globalObject, rutasObject){
+function  articleLineaLinkRuta(globalObject, lineaBaseInfoMatch, rutasObject){
   return `
   <article class="article__linkRuta">
     <h2 class="article__h2">Ruta Google Maps</h2>
-    <p class="article__p"><a href="${rutasObject[globalObject.SELECTED_COLEGIO_id][globalObject.ALL_SELECTIONS].linkRutaMaps}"> Enlace a ruta Maps 🔗</a></p>
+    <p class="article__p"><a target="_blank" href="${rutasObject[globalObject.SELECTED_LINEA_id][globalObject.ALL_SELECTIONS].linkRutaMaps}"> Enlace a ruta Maps 🔗</a></p>
   </article>
   `
 };
 
-function sectionObservaciones(globalObject, rutasObject){
-  if (rutasObject[globalObject.SELECTED_COLEGIO_id][globalObject.ALL_SELECTIONS].observaciones) {
+function articleLineaObservaciones(globalObject, lineaBaseInfoMatch, rutasObject){
+  if (rutasObject[globalObject.SELECTED_LINEA_id][globalObject.ALL_SELECTIONS].observaciones) {
     return `
     <article class="article__observaciones">
       <h2 class="article__h2">Observaciones</h2>
-      <p class="article__p">${rutasObject[globalObject.SELECTED_COLEGIO_id][globalObject.ALL_SELECTIONS].observaciones}</p>
+      <p class="article__p">${rutasObject[globalObject.SELECTED_LINEA_id][globalObject.ALL_SELECTIONS].observaciones}</p>
     </article>
     `
   } else {
@@ -128,17 +170,39 @@ function sectionObservaciones(globalObject, rutasObject){
   }
 };
 
-function sectionPartesTrabajo(globalObject, rutasObject){
+function articleLineaPartesTrabajo(globalObject, lineaBaseInfoMatch, rutasObject){
+  const parteTrabajo = lineaBaseInfoMatch.rutas[globalObject.SELECTED_DIAS_id].buses[globalObject.SELECTED_BUS_id].turno[globalObject.SELECTED_TURNO_id].parteTrabajo;
   return `
-    <article class="article__partesTrabajo">
-      <h2 class="article__h2">Parte de trabajo</h2>
+  <article class="article__partesTrabajo">
+    <h2 class="article__h2">Parte de trabajo</h2>
+    <div>
       <p class="article__p">Hora inicio</p>
-      <p class="article__p article__p--hrs">${rutasObject[globalObject.SELECTED_COLEGIO_id][globalObject.ALL_SELECTIONS].parteTrabajo.hrInicio}</p>
+      <p class="article__p article__p--hrs">${parteTrabajo.hrInicio}</p>
+    </div>
+    <div>
       <p class="article__p">Hora fin</p>
-      <p class="article__p article__p--hrs">${rutasObject[globalObject.SELECTED_COLEGIO_id][globalObject.ALL_SELECTIONS].parteTrabajo.hrFin}</p>
-    </article>
-  
-  `
+      <p class="article__p article__p--hrs">${parteTrabajo.hrFin}</p>
+    </div>
+    ${parteTrabajo.hrNoctur ? `
+      <div>
+        <p class="article__p">Horas noctur.</p>
+        <p class="article__p article__p--hrs">${parteTrabajo.hrNoctur}</p>
+      </div>
+    ` : ''}
+    ${parteTrabajo.diaFestivo ? `
+      <div>
+        <p class="article__p">Festivo</p>
+        <p class="article__p article__p--hrs">${parteTrabajo.diaFestivo}</p>
+      </div>
+    ` : ''}
+    ${parteTrabajo.pluses ? `
+      <div>
+        <p class="article__p">Pluses</p>
+        <p class="article__p article__p--pluses">${parteTrabajo.pluses}</p>
+      </div>
+    ` : ''}
+  </article>
+`;
 };
 
 function lineaStructure(globalObject, lineaBaseInfoMatch, rutasObject) {
@@ -148,6 +212,10 @@ function lineaStructure(globalObject, lineaBaseInfoMatch, rutasObject) {
     ${articleLineaJornada(globalObject, lineaBaseInfoMatch)}
     ${articleLineaBilletes(globalObject, lineaBaseInfoMatch)}
     ${articleLineaHorarios(globalObject, lineaBaseInfoMatch)}
+    ${articleLineaStops(globalObject, lineaBaseInfoMatch, rutasObject)}
+    ${articleLineaLinkRuta(globalObject, lineaBaseInfoMatch, rutasObject)}
+    ${articleLineaObservaciones(globalObject, lineaBaseInfoMatch, rutasObject)}
+    ${articleLineaPartesTrabajo(globalObject, lineaBaseInfoMatch, rutasObject)}
     `
 }
 
--- modules/rutasLineas.mjs
+++ modules/rutasLineas.mjs
@@ -1,18 +1,118 @@
-const paradasLinea = {
+const paradasLinea_OBJ = {
   'cs_almazora': {
     id: 'cs_almazora',
-    'cs_almazora_manana_rangoDias1': {
+    'cs_almazora-laborables-bus_1-manana': {
       stops: [
         {
-          name: '<br>[Aín] 🔗',
-          descript: '',
+          name: 'Pl. Juez Borrull<br>[Castellón] 🔗',
+          descript: 'Parada de buses',
           linkMaps: 'https://maps.app.goo.gl/ETNKJTywdqJuRvab8',
+        },
+        {
+          name: 'Av. Casalduch 52<br>[Castellón] 🔗',
+          descript: 'Bar Los Quintos',
+          linkMaps: 'https://maps.app.goo.gl/QcGvktNtF52QPTrp8',
+        },
+        {
+          name: 'Av. Valencia 28<br>[Castellón] 🔗',
+          descript: 'Cafetería Vecinos',
+          linkMaps: 'https://maps.app.goo.gl/rpKGE8aV1LYEhcb16',
+        },
+        {
+          name: 'Av. Valencia (Pl. Libertad)<br>[Castellón] 🔗',
+          descript: 'Media Luna',
+          linkMaps: 'https://maps.app.goo.gl/YcG4PNnVHLjg8Zek7',
+        },
+        {
+          name: 'Av. Valencia 110<br>[Castellón] 🔗',
+          descript: 'Ferrymas',
+          linkMaps: 'https://maps.app.goo.gl/YcG4PNnVHLjg8Zek7',
+        },
+        {
+          name: 'Av. Casalduch 140<br>[Castellón] 🔗',
+          descript: 'Periódico Mediterraneo',
+          linkMaps: 'https://maps.app.goo.gl/a22uPw8e9Z4miFCd9',
+        },
+        {
+          name: 'Av. Casalduch s/n<br>[Castellón] 🔗',
+          descript: 'Esq. Poligono Fadrell-Industrial',
+          linkMaps: 'https://maps.app.goo.gl/tVHyc1EgnZdezCAp9',
+        },
+        {
+          name: 'Ctra. Almazora<br>[Castellón] 🔗',
+          descript: 'Frente BOSCH',
+          linkMaps: 'https://maps.app.goo.gl/R5iEJLft4WU1PMSS9',
+        },
+        {
+          name: 'Ctra. Almazora<br>[Castellón] 🔗',
+          descript: 'Esq. Camino San Bernardo',
+          linkMaps: 'https://maps.app.goo.gl/bwW3zNhxwgK5hTEf6',
+        },
+        {
+          name: 'Grupo Perpetuo Socorro<br>[Almazora] 🔗',
+          descript: 'Esq. Camino San Bernardo',
+          linkMaps: 'https://maps.app.goo.gl/e9ZLq2nQM6d91Lu16',
+        },
+        {
+          name: 'Av. Castellón 237<br>[Almazora] 🔗',
+          descript: 'Rest. La Mancheguita',
+          linkMaps: 'https://maps.app.goo.gl/LvnqQuBhVKfxzmB28',
+        },
+        {
+          name: 'C/ San Fernando 105<br>[Almazora] 🔗',
+          descript: 'Telepizza',
+          linkMaps: 'https://maps.app.goo.gl/ZzkSTsDkQMoAHzSS6',
+        },
+        {
+          name: 'C/ San Fernando<br>[Almazora] 🔗',
+          descript: 'Frente Consum',
+          linkMaps: 'https://maps.app.goo.gl/uMkn4QLtX44VMPUo7',
+        },
+        {
+          name: 'Av. Constitución<br>[Almazora] 🔗',
+          descript: 'CEIP Errando Vilar',
+          linkMaps: 'https://maps.app.goo.gl/LbXmyvV94nEQRvVk6',
+        },
+        {
+          name: 'C/ Boqueras 107<br>[Almazora] 🔗',
+          descript: 'AquaHealth',
+          linkMaps: 'https://maps.app.goo.gl/WjgdetQnNuNmtACs8',
+        },
+        {
+          name: 'C/ Boqueras 95<br>[Almazora] 🔗',
+          descript: 'Frente Cervecería Quirón',
+          linkMaps: 'https://maps.app.goo.gl/9mP3nq3FtsKdRHCD6',
+        },
+        {
+          name: 'C/ L´Alcora 215<br>[Almazora] 🔗',
+          descript: 'Báscula',
+          linkMaps: 'https://maps.app.goo.gl/rFhz2CcTCfMLRUeJ8',
+        },
+        {
+          name: 'C/ L´Alcora 177<br>[Almazora] 🔗',
+          descript: 'Frente Forn de Juan',
+          linkMaps: 'https://maps.app.goo.gl/jbyMZ12a26D787vt8',
+        },
+        {
+          name: 'C/ L´Alcora 85<br>[Almazora] 🔗',
+          descript: 'Esq C/ San Luis',
+          linkMaps: 'https://maps.app.goo.gl/gu9De75eGnizc5iG7',
+        },
+        {
+          name: 'C/ Trinidad 37<br>[Almazora] 🔗',
+          descript: 'Caixa Almassora',
+          linkMaps: 'https://maps.app.goo.gl/6oXQzzMA5TZdmyzQ6',
+        },
+        {
+          name: 'Pl. Sta. Isabel<br>[Almazora] 🔗',
+          descript: 'Kebab',
+          linkMaps: 'https://maps.app.goo.gl/dvobRUUb54j6TGwz7',
         },
       ],
       observaciones: '',
-      linkRutaMaps: 'https://maps.app.goo.gl/Jfvn8bQPdwJWZUyJ7',
+      linkRutaMaps: 'https://maps.app.goo.gl/tsU6ZR95w3qTRkf98',
     },
   },
 };
 
-export { paradasLinea };
+export { paradasLinea_OBJ };
